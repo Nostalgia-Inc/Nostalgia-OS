fc28f12ae8b889bcae54673047614bd59d156c5e
MrOgieBogie <paulthealien921@gmail.com>
2025-08-02 16:01:59 +1000

    update container and sh

--- images/nostalgia-crt/firstboot/10-nostalgia-setup.sh
+++ images/nostalgia-crt/firstboot/10-nostalgia-setup.sh
@@ -13,6 +13,14 @@ fi
 # Custom hostname
 hostnamectl set-hostname "$(hostname)_CRT"
 
+# Flatpak setup for Arduino IDE
+echo "[Nostalgia CRT] Installing Flatpak apps..."
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y --noninteractive flathub cc.arduino.arduinoide
+
+# Optionally add EmuDeck or any other setup here in the future
+
+# Disable service so it doesn't re-run
 systemctl disable nostalgia.service
 
 echo "[Nostalgia CRT] First boot setup completed."
